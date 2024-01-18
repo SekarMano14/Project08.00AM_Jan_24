a88d13b44ca7a3c7d7116cae65111554a5e54a81
SekarMano14 <sekarmano46@gmail.com>
2024-01-18 08:14:05 +0530

    Mano's new Code

--- src/test/java/org/test/MainTest.java
+++ src/test/java/org/test/MainTest.java
@@ -4,6 +4,8 @@ public class MainTest {
 	public static void main(String[] args) {
 		//here can start our code
 		System.out.println("Test1");
+		System.out.println("Test4");
+		System.out.println("Test5");
 	}
 
 }
